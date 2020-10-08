f5b81606aacc96282d671e4da2ed52192a7cb6bb
xenomorphling <mixtapeking@mail.ru>
2020-10-08 18:53:46 +0300

    quiz

---
+++ week4/week4quiz.py
@@ -0,0 +1,9 @@
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate newfilenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+
+newfilenames = []
+
+
+print(newfilenames) 
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
