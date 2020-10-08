cbf28030221893afa86f089acd90a0c679a3f7a5
xenomorphling <mixtapeking@mail.ru>
2020-10-08 20:05:04 +0300

    quiz

--- week4/week4quiz.py
+++ week4/week4quiz.py
@@ -2,8 +2,21 @@ filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.o
 # Generate newfilenames as a list containing the new filenames
 # using as many lines of code as your chosen method requires.
 
-newfilenames = []
+# newfilenames = []
+# print(type(newfilenames))
+# for index, filename in enumerate(filenames)
+# (filenames):
+#   filename = filename.replace(".hpp", ".h")
+#   print(filename)
+#   newfilenames = newfilenames.insert(index, filename)
 
-
-print(newfilenames) 
-# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
+# print(newfilenames) 
+# # Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
+ 
+newfilename = []
+print(type(newfilename))
+for index, filename in enumerate(filenames):
+  print(index, filename)
+  filename = filename.replace(".hpp", ".h")
+  print(filename)
+  
