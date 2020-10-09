8336b1ad0f14d38e3956662b5ade9358113cb2ff
xenomorphling <mixtapeking@mail.ru>
2020-10-09 16:23:09 +0300

    quiz

--- week4/week4quiz.py
+++ week4/week4quiz.py
@@ -3,6 +3,6 @@ filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.o
 # using as many lines of code as your chosen method requires.
 newfilenames = ([])
 for index, filename in enumerate(filenames):
-  newfilenames == newfilenames.append(filename.replace(".hpp", ".h"))
-print(newfilenames) 
+    var = newfilenames == newfilenames.append(filename.replace(".hpp", ".h"))
+print(newfilenames)
 # Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
