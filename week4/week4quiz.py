e39fad2dbed26dd47486d3332eb152425079dc14
Yury Matsyuk <ymatsyuk@luxoft.com>
2021-03-01 14:02:44 +0300

    second task done

--- week4/week4quiz.py
+++ week4/week4quiz.py
@@ -1,26 +1,22 @@
-filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
-# Generate newfilenames as a list containing the new filenames
-# using as many lines of code as your chosen method requires.
-newfilenames = ([])
-for index, filename in enumerate(filenames):
-    var = newfilenames == newfilenames.append(filename.replace(".hpp", ".h"))
-print(newfilenames)
-# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
+# filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# # Generate newfilenames as a list containing the new filenames
+# # using as many lines of code as your chosen method requires.
+# newfilenames = ([])
+# for index, filename in enumerate(filenames):
+#     var = newfilenames == newfilenames.append(filename.replace(".hpp", ".h"))
+# print(newfilenames)
+# # Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
 
 
-# Question 2
 
-# Let's create a function that turns text into pig latin: a simple text transformation that modifies each word moving the first character to the end and appending "ay" to the end. For example, python ends up as ythonpay.
+# def pig_latin(text):
+#   say = ""
+#   words = text.split()
+#   for word in words:
+#     say += ("" + word[1:] + word[0] + "ay ")
+#   return(say)
 
-def pig_latin(text):
-  say = ""
-  # Separate the text into words
-  words = ___
-  for word in words:
-    # Create the pig latin word and add it to the list
-    ___
-    # Turn the list back into a phrase
-  return ___
-		
-print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
-print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
+# print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+# print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
+
+
